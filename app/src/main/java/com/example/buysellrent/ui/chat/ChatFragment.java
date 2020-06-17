a927693ef2191eab5c222385fa795f846beff676
yashgoenka01 <goenka.yash0@gmail.com>
2020-06-17 18:05:42 +0545

    Fragments of Chat remaining

--- app/src/main/java/com/example/buysellrent/ui/chat/ChatFragment.java
+++ app/src/main/java/com/example/buysellrent/ui/chat/ChatFragment.java
@@ -8,28 +8,76 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.buysellrent.Adapter.SectionPagerAdapter;
 import com.example.buysellrent.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
+import static androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 public class ChatFragment extends Fragment {
 
-    private ChatViewModel chatViewModel;
+    //private ChatViewModel chatViewModel;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    View root;
+
+    public ChatFragment(){
+
+    }
+    public static  ChatFragment getInstance()
+    {
+        return new ChatFragment();
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        chatViewModel =
-                ViewModelProviders.of(this).get(ChatViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_chat, container, false);
-        final TextView textView = root.findViewById(R.id.text_chat);
-        chatViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        root = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        tabLayout=root.findViewById(R.id.chat_tab);
+        viewPager=root.findViewById(R.id.chat_view);
+
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setUpViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-        return root;
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        SectionPagerAdapter adapter=new SectionPagerAdapter(getChildFragmentManager(),BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        adapter.addFragment(new Buy_zone(),"BUYING");
+        adapter.addFragment(new Sell_zone(),"SELLING");
+        viewPager.setAdapter(adapter);
     }
 }
