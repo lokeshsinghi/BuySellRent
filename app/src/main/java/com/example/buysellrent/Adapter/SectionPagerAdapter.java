d2ca4b5144aba60d87723153bed0307adc055e27
yashgoenka01 <goenka.yash0@gmail.com>
2020-06-17 17:51:59 +0545

    Fragments of Chat

---
+++ app/src/main/java/com/example/buysellrent/Adapter/SectionPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.buysellrent.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<Fragment> fragments=new ArrayList<>();
+    private ArrayList<String> titles=new ArrayList<>();
+
+    public SectionPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+    public void addFragment(Fragment fragment,String title) {
+        fragments.add(fragment);
+        titles.add(title);
+
+    }
+}
---
+++ app/src/main/java/com/example/buysellrent/ui/chat/Buy_zone.java
@@ -0,0 +1,26 @@
+package com.example.buysellrent.ui.chat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.buysellrent.R;
+
+
+public class Buy_zone extends Fragment {
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_buy_zone, container, false);
+    }
+}
